7b84378ca0f1e64c84312120285cac755208a95f
Marios Avgeris <avgeris.marios@gmail.com>
2018-07-18 17:04:45 +0300

    implement offloading behaviour for 2 apps

--- api/views/forwarder.py
+++ api/views/forwarder.py
@@ -16,52 +16,49 @@ import requests
 def post(request, slug=None):
     # TODO: test for asynchronous requests
     # slug == app_id
-    if slug == "0":
+    if slug == "0" or slug == "1":
         app = App.objects.get(app_id=slug)
         containers_op_list = app.get_containers_op_list()
-        print str(containers_op_list)
+        print ("Operating Points of Containers currently running App " + slug + " : "  + str(containers_op_list))
         # Match operating points to request processing capabilities
         request_procc_cap = map(lambda op: settings.GLOBAL_SETTINGS['U_REQ_MAX'][int(slug)][op], containers_op_list)
-        print str(request_procc_cap)
+        print ("Respective request processing capabilities: " + str(request_procc_cap))
         # Create total request capabilities
         total_requests_capability = sum(request_procc_cap)
-        print total_requests_capability
+        print ("Total request processing capabilities: " + str(total_requests_capability))
         # Create possibilities by dividing each request capability with total requests capabilities
         possibilities = map(lambda pc: pc/total_requests_capability, request_procc_cap)
-        print possibilities
+        print ("Respective possibilities of the request being offloaded to container: " + str(possibilities))
         # Create a list containing the cumulative sum of the possibilities
         cumsum_possibilities = reduce(lambda c, x: c + [c[-1] + x], possibilities, [0])[1:]
-        print cumsum_possibilities
+        print ("Respective cumulative sum of possibilities: " + str(cumsum_possibilities))
         # Seed a random uniform number
         rand = random.uniform(0, 1)
-        print rand
+        print ("Random number to select container: " + str(rand))
         # Offload request to container dictated by rand
-        container_id = next(i for i, v in enumerate(cumsum_possibilities) if v > rand)
-        print container_id
-        # app0 is running on :8800 & app1 on 8801
-        resp = offload(slug, container_id, request)
-        return Response(resp)
-        # return Response(status=status.HTTP_204_NO_CONTENT)
-    elif slug == "1":
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        host_id = next(i for i, v in enumerate(cumsum_possibilities) if v > rand)
+        print ("Actual selected Host ID: " + str(host_id))
+        # app0 is running on :8001 & app1 on 8002
+        resp = offload(slug, host_id, request)
+        return Response({'Response' : resp})
     else:
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-def offload(app_id, container_id, request):
+def offload(app_id, host_id, request):
     size = request.data['size']
-    print size
+    print ("Size: " + str(size) + "B")
     pts = request.data['start_time']
-    print pts
+    print ("Timestamp: " + str(pts))
     img = request.data['file']
-    print img.name
+    print ("Image name: " + img.name)
+    print ("------------------------------------------------------------\n")
     json = {"size": size, "start_time": pts}
     files = {"file": img}
-    # post_url = "http://0.0.0.0:880" + str(container_id) + "/ca_tf/imageUpload/" + file.name
-    # TODO change from localhost & correct port
-    post_url = "http://0.0.0.0:8000/ca_tf/imageUpload/" + img.name
+    post_url = "http://" + settings.GLOBAL_SETTINGS['HOST_IPS'][host_id]  + ":" + str(8001+int(app_id)) + "/ca_tf/imageUpload/" + img.name
     try:
         return requests.post(post_url, files=files, data=json, timeout=15)
-    except requests.Timeout:
-        print('Host unavailable.')
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    except (requests.Timeout, requests.ConnectionError):
+        print('Host at ' + settings.GLOBAL_SETTINGS['HOST_IPS'][host_id]  + ' unavailable.')
+        #return Response({'Response': status=status.HTTP_404_NOT_FOUND})
+        return 'Host unavailable.'
